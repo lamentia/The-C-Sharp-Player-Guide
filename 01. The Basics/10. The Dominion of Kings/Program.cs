7942ba89184032fc8c45d4e6c033ed919c61d12c
Elliot Alderson <53076000+lamentia@users.noreply.github.com>
2022-06-02 18:00:25 +0900

    Moving through the basics

---
+++ 01. The Basics/10. The Dominion of Kings/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// This solution is overcomplicated!
+
+
+//Introduction
+
+Console.WriteLine("What's your name, you highness?");
+string kingName = Console.ReadLine();
+
+//Checking your estates points
+
+Console.WriteLine("King " + kingName + ", how many estates you have?");
+string firstEquality = Console.ReadLine();
+int totalEstates = Convert.ToInt32(firstEquality);
+totalEstates*=1;
+
+//Checking your duchy points
+
+Console.WriteLine("King " + kingName + ", how many duchies you have?");
+string secondEquality = Console.ReadLine();
+int totalDuchies = Convert.ToInt32(secondEquality);
+totalDuchies*=3;
+
+//Checking your provinces points
+
+Console.WriteLine("King " + kingName + ", how many provinces you have?");
+string thirdEquality = Console.ReadLine();
+int totalProvinces = Convert.ToInt32(thirdEquality);
+totalProvinces*=6;
+
+//Calculating your total
+
+Console.WriteLine("King " + kingName + ", your total points is " + (totalEstates + totalDuchies + totalProvinces) + "!");
+Console.ReadKey();
+
+// This solution is overcomplicated!
---
+++ 01. The Basics/11. The Defense of Consolas/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Title = "Defense of Shu Kingdom";
+
+Console.WriteLine("Press any key when you're ready to defend.");
+Console.ReadKey(true);
+Console.ForegroundColor = ConsoleColor.Red;
+
+Console.WriteLine("The war is upon us, milord! Please gather your troops and command it to avoid damage! ");
+
+Console.Write($"Our scouts telling us they will hit Target Row! ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write($"And also Target Column! ");
+int column = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nZhuge Liang is saying we should choose where to move our army!");
+string targetDeployment = Console.ReadLine();
+Console.ForegroundColor = ConsoleColor.Yellow;
+
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})\n");
+Console.WriteLine("What will be your order, milord?");
+
+string deploymentWin = Console.ReadLine();
+Console.Beep(440, 1000);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write($"\n{deploymentWin} We saved our army!");
+Console.ReadKey();
