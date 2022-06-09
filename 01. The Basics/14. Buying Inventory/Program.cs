af42e70bea982fa585255d9f14c525576a310170
Elliot Alderson <elsbethlamentia@gmail.com>
2022-06-09 16:20:28 +0900

    Going with the flow

--- 01. The Basics/14. Buying Inventory/Program.cs
+++ 01. The Basics/14. Buying Inventory/Program.cs
@@ -1,5 +1,9 @@
-﻿/* Choice Number 1 
-Console.WriteLine("The following items are available:");
+﻿//Choice Number 1 
+
+Console.Write("What's your name, young adventurer?");
+var name = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine($"1 - {"Rope"}");
 Console.WriteLine($"2 - {"Torches"}");
 Console.WriteLine($"3 - {"Climbing Equipment"}");
@@ -8,37 +12,73 @@ Console.WriteLine($"5 - {"Machete"}");
 Console.WriteLine($"6 - {"Canoe"}");
 Console.WriteLine($"7 - {"Food Supplies"}");
 
+if (name == "Link")
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Ahhh, {name} it's you! The following items are available for half of price for you!:");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{name}! The following items are available:");
+}
+
 Console.Write("What number do you want to see the price of?");
 int choice = Convert.ToInt32(Console.ReadLine());
 
-switch(choice)
+
+string item = choice switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies"
+};
+
+int price = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 1
+};
+if (name == "Link")
+    price /= 2;
+
+switch (choice)
 {
     case 1:
-        Console.WriteLine("Rope cost 10 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 2:
-        Console.WriteLine("Torches cost 15 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 3:
-        Console.WriteLine("Climbing Equipment cost 25 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 4:
-        Console.WriteLine("CLean Water cost 1 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 5:
-        Console.WriteLine("Machete cost 20 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 6:
-        Console.WriteLine("Canoe cost 200 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
     case 7:
-        Console.WriteLine("Food Supplies cost 1 gold.");
+        Console.WriteLine($"{item} cost {price} gold.");
         break;
 }
 Console.ReadKey();
-*/
 
-// Choice number 2
+
+/* Choice number 2
 
 Console.WriteLine("The following items are available:");
 Console.WriteLine("1 - Rope");
@@ -76,3 +116,4 @@ int price = item switch
 
 Console.WriteLine($"{item} cost {price} gold.");
 Console.ReadKey();
+*/
