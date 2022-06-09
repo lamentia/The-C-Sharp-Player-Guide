22cd29a8ac0097d4a941bdd784a530b6880a1bbe
Elliot Alderson <elsbethlamentia@gmail.com>
2022-06-09 23:10:33 +0900

    Going with the flow

---
+++ 01. The Basics/14-15. Buying Inventory +Discounted Inventory/Program.cs
@@ -0,0 +1,119 @@
+﻿//Choice Number 1 
+
+Console.Write("What's your name, young adventurer?");
+var name = Console.ReadLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"1 - {"Rope"}");
+Console.WriteLine($"2 - {"Torches"}");
+Console.WriteLine($"3 - {"Climbing Equipment"}");
+Console.WriteLine($"4 - {"Clean Water"}");
+Console.WriteLine($"5 - {"Machete"}");
+Console.WriteLine($"6 - {"Canoe"}");
+Console.WriteLine($"7 - {"Food Supplies"}");
+
+if (name == "Link")
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"Ahhh, {name} it's you! The following items are available for half of price for you!:");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{name}! The following items are available:");
+}
+
+Console.Write("What number do you want to see the price of?");
+int choice = Convert.ToInt32(Console.ReadLine());
+
+
+string item = choice switch
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies"
+};
+
+int price = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 1
+};
+if (name == "Link")
+    price /= 2;
+
+switch (choice)
+{
+    case 1:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 2:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 3:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 4:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 5:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 6:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+    case 7:
+        Console.WriteLine($"{item} cost {price} gold.");
+        break;
+}
+Console.ReadKey();
+
+
+/* Choice number 2
+
+Console.WriteLine("The following items are available:");
+Console.WriteLine("1 - Rope");
+Console.WriteLine("2 - Torches");
+Console.WriteLine("3 - Climbing Equipment");
+Console.WriteLine("4 - Clean Water");
+Console.WriteLine("5 - Machete");
+Console.WriteLine("6 - Canoe");
+Console.WriteLine("7 - Food Supplies");
+
+Console.Write("What number do you want to see the price of?");
+int itemNumber = Convert.ToInt32(Console.ReadLine());
+string item = itemNumber switch
+
+{
+    1 => "Rope",
+    2 => "Torches",
+    3 => "Climbing Equipment",
+    4 => "Clean Water",
+    5 => "Machete",
+    6 => "Canoe",
+    7 => "Food Supplies"
+};
+
+int price = item switch
+{
+    "Rope" => 10,
+    "Torches" => 15,
+    "Climbing Equipment" => 25,
+    "Clean Water" => 1,
+    "Machete" => 20,
+    "Canoe" => 200,
+    "Food Supplies" => 1
+};
+
+Console.WriteLine($"{item} cost {price} gold.");
+Console.ReadKey();
+*/
---
+++ 01. The Basics/16. The Prototype/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Title = "Guessing Game";
+// Getting pilot name
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Please enter your name: ");
+string pilotName = Console.ReadLine();
+Console.WriteLine($"{pilotName}, you are a pilot!");
+Console.Beep();
+
+//Getting a guessing number by pilot
+int pilotNumber;
+do
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.Write($"{pilotName}, please enter a number between 0 and 100: ");
+    string pilotResponse = Console.ReadLine(); 
+    pilotNumber = Convert.ToInt32(pilotResponse);
+}
+while (pilotNumber < 0 || pilotNumber > 100);
+Console.Clear();
+
+
+//Getting hunter name
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.Write("Please enter your name: ");
+string hunterName = Console.ReadLine();
+Console.WriteLine($"{hunterName}, you are a hunter!");
+Console.Beep();
+
+//Guessing the number by hunter
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write($"{hunterName}, please guess the number in order to eat the hunter: ");
+    string hunterResponse = Console.ReadLine();
+    if (hunterResponse == "quit" || hunterResponse == "exit")
+        break;
+
+    int hunterNumber = Convert.ToInt32(hunterResponse);
+
+    if (hunterNumber > pilotNumber)
+    {
+        Console.WriteLine($"{hunterName}, {hunterNumber} is too high!");
+        Console.Write("What is your next guess? ");
+        continue;
+    }
+    else if (hunterNumber < pilotNumber)
+    {
+        Console.WriteLine($"{hunterName}, {hunterNumber} is too low!");
+        Console.Write("What is your next guess? ");
+        continue;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Beep();
+        Console.WriteLine($"{hunterName}, you guessed it! You ate the hunter!");
+        break;
+    }
+}
+Console.ReadKey();
